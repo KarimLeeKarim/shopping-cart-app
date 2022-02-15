035cb8b6661cd901c8bedcf96f36c8df5ef8163c
KarimLeeKarim <kerimlikerim24@gmail.com>
2022-02-15 17:25:28 +0400

    Updates

--- src/store/store.tsx
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore, Store } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import ReduxThunk from "redux-thunk";
-import reducer from "../redux/reducers/reducer";
-
-const store: Store<DataState, DataAction> = createStore(reducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
-
-export default store;
